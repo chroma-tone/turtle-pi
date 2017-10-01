3ff083ec5e1ef2b076ff39d97f4e6f91dea68913
Jeremy Gold <jeremy.gold@ikegps.com>
2017-10-01 21:11:12 +1300

    Initial implementation - ready to try out on the motor controller board.

--- sr-motor-control.py
+++ sr-motor-control.py
@@ -3,6 +3,9 @@ import Rpi.GPIO.LOW as LOW
 import Rpi.GPIO.HIGH as HIGH
 
 class ShiftRegisterMotorControl:
+    # Static state of latch across all ShiftRegisterMotorControl instances
+    latchState = 0
+
     # Bit positions in the 74HCT595 shift register output
     MOTOR1_A = 2
     MOTOR1_B = 3
@@ -19,22 +22,60 @@ class ShiftRegisterMotorControl:
     BRAKE = 3
     RELEASE = 4
 
-    # Arduino pin names for interface to 74HCT595 latch
-    MOTORLATCH = 12 # TOOD
-    MOTORCLK = 4 # TOOD
-    MOTORENABLE = 7 # TOOD
-    MOTORDATA = 8 # TOOD
+    # RPI pins: Ref https://www.element14.com/community/servlet/JiveServlet/previewBody/73950-102-11-339300/pi3_gpio.png
+    # Interface to 74HCT595 latch
+    MOTORLATCH = 5
+    MOTORCLK = 6
+    MOTORENABLE = 13
+    MOTORDATA = 19
+
+    # PWM motor speed control
+    MOTOR1_EN = 12
+    MOTOR2_EN = 16
+    MOTOR3_EN = 20
+    MOTOR4_EN = 21
+
+    # PWM control frequency (Hz)
+    PWM_FREQ = 100
 
     def __init__(this, motorNumber):
-        this.motorNumber = motorNumber
+        if motorNumber > 4 || motorNumber < 1:
+            raise ValueError, "motorNumber must be between 1 and 4 inclusive"
 
-    def latch_tx(value):
+        if motorNumber == 1:
+            this.motorA = MOTOR1_A
+            this.motorB = MOTOR1_B
+            this.motorPwm = GPIO.PWM(MOTOR1_EN, PWM_FREQ)
+
+        elif motorNumber == 2:
+            this.motorA = MOTOR2_A
+            this.motorB = MOTOR2_B
+            this.motorPwm = MOTOR2_EN
+            this.motorPwm = GPIO.PWM(MOTOR2_EN, PWM_FREQ)
+
+        elif motorNumber == 3:
+            this.motorA = MOTOR3_A
+            this.motorB = MOTOR3_B
+            this.motorPwm = MOTOR3_EN
+            this.motorPwm = GPIO.PWM(MOTOR3_EN, PWM_FREQ)
+
+        elif motorNumber == 4:
+            this.motorA = MOTOR4_A
+            this.motorB = MOTOR4_B
+            this.motorPwm = GPIO.PWM(MOTOR4_EN, PWM_FREQ)
+
+        # Initialize motor driver to idle state
+        this.motorPwm.start(0)
+        this.setDirection(RELEASE)
+        latch_tx()
+
+    def latch_tx():
         GPIO.output(MOTORLATCH, LOW)
         GPIO.output(MOTORDATA, GPIO.LOW)
 
         for i in range(0,7):
             GPIO.output(MOTORCLK, LOW)
-            if (value >> i) & 0x01 == 0x01:
+            if (latchState >> i) & 0x01 == 0x01:
                 GPIO.output(MOTORDATA, HIGH)
             else:
                 GPIO.output(MOTORDATA, LOW)
@@ -42,10 +83,19 @@ class ShiftRegisterMotorControl:
 
         GPIO.output(MOTORLATCH, HIGH)
 
-    def run(control):
+    def setDirection(direction):
+        if direction == FORWARD:
+            this.latchState |= 1 << this.motorA
+            this.latchState &= !(1 << this.motorB)
+        if direction == BACKWARD:
+            this.latchState &= !(1 << this.motorA)
+            this.latchState |= 1 << this.motorB
+        if direction == RELEASE:
+            this.latchState &= !(1 << this.motorA)
+            this.latchState &= !(1 << this.motorB)
 
+        latch_tx()
 
-
-    def setSpeed(control):
-        # TODO: Set corresponding pin pwm rate
+    def setSpeed(percentSpeed):
+        this.motorPwm.ChangeDutyCycle(percentSpeed)
 
